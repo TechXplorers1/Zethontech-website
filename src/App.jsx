eb68a58b2f9d6676f6d0e612d47369e7efe17598
Humer-r5 <reshamhumer.tx@gmail.com>
2025-04-08 12:30:09 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,24 @@ function App() {
   return (
     <>
     <div className="hero-section">
-      <Navbar bg="transparent" expand="lg" className="navbar-custom px-4 pt-3">
-        <Navbar.Brand href="#home">
-          <img src={logo} alt="TechXplorers Logo" height="40" />
-        </Navbar.Brand>
-        <Nav id="navbar" className="mx-auto">
-          <Nav.Link href="#home" className="text-black mx-2 nav-link">Home</Nav.Link>
-          <Nav.Link href="#about" className="text-black mx-2 nav-link">About</Nav.Link>
-          <Nav.Link href="#services" className="text-black mx-2 nav-link">Services</Nav.Link>
-          <Nav.Link href="#contact" className="text-black mx-2 nav-link">Contact</Nav.Link>
-        </Nav>
-      </Navbar>
-      <div className="cinematic-text-wrapper">
-        <h1 className="cinematic-text">Welcome to Techxplorers private limited</h1>
-      </div>
-    </div>
+  <Navbar bg="transparent" expand="lg" className="navbar-custom">
+    <Navbar.Brand href="#home">
+      <img src={logo} alt="TechXplorers Logo" height="40" />
+    </Navbar.Brand>
+    <Nav id="navbar" className="mx-auto">
+      <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+      <Nav.Link href="#about" className="nav-link">About</Nav.Link>
+      <Nav.Link href="#services" className="nav-link">Services</Nav.Link>
+      <Nav.Link href="#contact" className="nav-link">Contact</Nav.Link>
+    </Nav>
+  </Navbar>
+
+  <div className="cinematic-text-wrapper">
+    <h1 className="cinematic-text">
+      Welcome<br />To<br />TechXplorers Private Limited
+    </h1>
+  </div>
+</div>
 
 <div className="content-section">
 <Container>
