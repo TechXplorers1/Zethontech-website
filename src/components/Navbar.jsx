7e993dd45ab16eab10a855edf0f2f8689b7e7991
Humer-r5 <reshamhumer.tx@gmail.com>
2025-04-10 17:18:54 +0530

    Updated Services

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../assets/logo.png';
 import '../styles/navbar.css';
-import Services from './Services';
+import ServicesDropdown from './Services';
 
 const CustomNavbar = ({ scrolled }) => {
   const [showPopup, setShowPopup] = useState(false);
@@ -17,18 +17,26 @@ const CustomNavbar = ({ scrolled }) => {
         <Navbar.Brand href="#home">
           <img src={logo} alt="TechXplorers Logo" height="40" />
         </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto align-items-center">
             <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
+
             <div
               className="nav-link services-popup-wrapper"
               onMouseEnter={() => setShowPopup(true)}
               onMouseLeave={() => setShowPopup(false)}
+              style={{ position: 'relative' }}
             >
               <span>Services</span>
-              {showPopup && <Services />}
+              {showPopup && (
+                <div style={{ position: 'absolute', top: '100%', left: 0, width: '100vw' }}>
+                  <ServicesDropdown />
+                </div>
+              )}
             </div>
+
             <Nav.Link href="#industries" className="nav-link">Industries</Nav.Link>
             <Nav.Link href="#about" className="nav-link">About</Nav.Link>
             <Nav.Link href="#careers" className="nav-link">Careers</Nav.Link>
